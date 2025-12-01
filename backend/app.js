7d6ba4c532664996dad99d9aa99524d37a808617
Runner-Dad84 <vanhoogie@gmail.com>
2025-11-30 20:56:43 -0500

    Add routes and passport-local strategy

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const configurePassport = require("./passportConfig");
 const path = require("path");
+const userRouter = required("./routes/userRouter")
+const postRouter = required("./routes/userRouter")
 
 const app = express();
 const PORT = process.env.PORT || 3011;
@@ -9,10 +14,27 @@ const PORT = process.env.PORT || 3011;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+// Session middleware (required for passport)
+app.use(
+  session({
+    secret: "supersecretkey",
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Configure passport strategies
+configurePassport(passport);
+
 // routes
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+app.use("/", userRouter);
+app.use("/", postRouter);
 
 // start server
 app.listen(PORT, () => {
