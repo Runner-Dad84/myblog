026cb3a4b7b0b4bf4a7ab285fa86c37e41d31ac9
Runner-Dad84 <vanhoogie@gmail.com>
2025-12-21 21:51:51 -0500

    Move patch logic into controller

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -41,7 +41,7 @@ async function deletePost(req, res){
         if (post.userId !== userId) {
           return res.status(403).json({ error: "Not authorized to delete this post" });
         }
-        
+
         await prisma.comment.deleteMany({
              where: { postId },
         });
@@ -55,8 +55,64 @@ async function deletePost(req, res){
             res.status(500).send("Server error");
         };
 }
+///working on this now
+async function editPost(req, res) {
+        const postId = parseInt(req.params.id, 10);
+        const userId = req.user?.id;
+
+        if (Number.isNaN(postId)) {
+            return res.status(400).send("Invalid post id");
+        }
+        
+        try {
+            //Find the specific post
+            const post = await prisma.post.findUnique({
+                 where: { id: postId },
+            });
+             //make sure post exists
+            if (!post) {
+                return res.status(404).send("Post not found");
+            }
+             // Prevent any user from editing any other user's post
+            if (post.userId !== userId) {
+                return res.status(403).send("Not authorized to edit this post");
+            }
+            //updated post
+            const updates = {};
+    
+            if (req.body.title !==  undefined){
+                   updates.title = String(req.body.title).trim();
+            }
+             if (req.body.content !==  undefined){
+                   updates.content = String(req.body.content).trim();
+            }
+             if (req.body.isPublic !==  undefined){
+                   updates.isPublic = req.body.isPublic; 
+            }
+             if (req.body.isPublished !==  undefined){
+                   updates.isPublished = req.body.isPublished; 
+            }
+            //if no updates made do not update
+            if (Object.keys(updates).length === 0) {
+                return res.status(400).send("No updates entered");
+            }
+
+            //replace the specific post with the update post
+            const updatedPost = await prisma.post.update({
+                where: { id: postId },
+                data: updates
+            });
+            //finally return updated post
+            res.json(updatedPost)
+    
+        } catch (err) {
+            console.log(err)
+            res.status(500).send("Server error");
+        }
+}
 
 module.exports = {
   createPost,
-  deletePost
+  deletePost,
+  editPost
 };
