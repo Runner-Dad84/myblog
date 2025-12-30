a78b4b4772d63f1583cf3619045cf549ed04da10
Runner-Dad84 <vanhoogie@gmail.com>
2025-12-29 22:25:23 -0500

    middleware confirm post owner match

---
+++ backend/middleware/postOwner.js
@@ -0,0 +1,8 @@
+function postOwner(req, res, next) {
+  if (req.post.userId !== req.user.id) {
+    return res.status(403).json({ error: "Not authorized" });
+  }
+  next();
+}
+
+module.exports = postOwner;
