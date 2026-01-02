951157ddbf2f2d142252e00bd2fd913e081ed561
Runner-Dad84 <vanhoogie@gmail.com>
2026-01-01 22:00:12 -0500

    delete comment controller

---
+++ backend/controllers/commentController.js
@@ -0,0 +1,43 @@
+const prisma = require("../prismaClient");
+
+
+async function createComment(req, res) {
+    try {
+    const { content } = req.body;
+    const userId = req.user.id;
+    const postId = parseInt(req.params.postId, 10);
+
+      const newComment = await prisma.comment.create({
+      data: {
+        content,
+        userId,
+        postId,
+      },
+    });
+     res.status(201).json({ message: "Comment created", comment: newComment });
+        } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
+}
+
+///working here
+async function deleteComment(req, res){
+    try {
+
+        await prisma.comment.delete({
+            where: { id: req.comment.id },
+         });
+         res.status(200).json({ message: "Comment deleted" });
+        } catch (err) {
+            console.log(err)
+            res.status(500).send("Server error");
+        };
+}
+
+
+module.exports = {
+  createComment,
+  deleteComment,
+  editComment
+};
