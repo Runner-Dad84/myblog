10ce56759a8079a7aa37e569d3dfac72fe5b1534
Runner-Dad84 <vanhoogie@gmail.com>
2026-01-01 21:57:17 -0500

    comment delete middleware

---
+++ backend/middleware/commentOwner.js
@@ -0,0 +1,12 @@
+//Requestor must be the commentor or the poster
+function commentOwner(req, res, next) {
+    const commentOwner = req.comment.userId === req.user.id;
+    const postOwner = req.post.userId === req.user.id;
+    
+  if (!postOwner && !commentOwner) {
+    return res.status(403).json({ error: "Not authorized" });
+  }
+  next();
+}
+
+module.exports = commentOwner;
---
+++ backend/middleware/loadComment.js
@@ -0,0 +1,25 @@
+// middleware/loadPost.js
+const prisma = require('../prismaClient');
+
+async function loadPost(req, res, next) {
+  const commentId = parseInt(req.params.id || req.params.commentId, 10);
+
+  if (Number.isNaN(commentId)) {
+    return res.status(400).json({ error: "Invalid comment ID" });
+  }
+
+  const comment = await prisma.comment.findUnique({
+    where: { id: commentId },
+  });
+
+  if (!comment) {
+    return res.status(404).json({ error: "Post not found" });
+  }
+
+  req.comment = comment;
+  req.post = comment.post;
+  
+  next();
+}
+
+module.exports = loadComment;
