9036954b70bfc9ae29020779d64ae6bfbd829f55
Runner-Dad84 <vanhoogie@gmail.com>
2026-01-29 22:33:09 -0500

    Ondelete button to remove post

--- frontend-admin/pages/AdminPostPage.jsx
+++ frontend-admin/pages/AdminPostPage.jsx
@@ -4,7 +4,7 @@ import PostList from '../../components/PostList'
 const HomePageContainer = () => {
     const [posts, setPosts] = useState([]);
     const [isLoading, setIsLoading] = useState(true)
-    const [error, setError] = useState(true)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
     // Data fetching or other business logic
@@ -14,6 +14,11 @@ const HomePageContainer = () => {
         if (!response.ok){
             throw new Error('failed to fetch posts')
         }
+
+        <button
+      onClick={() => onDelete(post.id)}
+      >Delete Post</button>
+    
 
 
         const data = await response.json();
@@ -26,21 +31,34 @@ const HomePageContainer = () => {
         setIsLoading(false);
       }
     }
+
     fetchPosts();
 }, []);
 
-    
+const handleDelete = async (postId) => {
+    try {
+        await fetch(`/api/post/${postId}`, {
+            method: 'DELETE',
+        });
+setPosts(prev => prev.filter(p => p.id !== postId));
+} catch (err) {
+    console.error('Delete failed', err);
+}
+};
   
-
   if (isLoading) {return <div>Loading...</div>;}
   if (error) return <div>Error: {error}</div>;
 
+// This container orchestrates layout and passes data
   return (
-    // This container orchestrates layout and passes data
     <div className="adminPostPage-container">
       <h1>Admin Page</h1>
-      <PostList posts={posts} />
+      <PostList posts={posts} onDelete={handleDelete}/>
+
     </div>
+
+
+      
   );
 }
 export default AdminPostPage;
