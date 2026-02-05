3d13996d6883502bcf25c1c1ffc4c08251c3c64a
Runner-Dad84 <vanhoogie@gmail.com>
2026-02-04 22:07:13 -0500

    Rename frontend-admin to just frontend

---
+++ frontend/components/PostList.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './PostList.css'; //
+
+const PostList = ({ 
+    posts = [],
+    onDelete,
+    onEdit,
+    isAdmin = false
+}) => {
+
+    const list = posts.map((post) => 
+        <li key = {post.id}>
+            Title: {post.title}
+            Author: {post.user.username}
+            Posted: {post.generatedAt}
+            Coments: {post.comments.length}
+            Public: {post.isPublic ? 'Yes' : 'No'}
+            Visible: {post.isPublished ? 'Yes' : 'No'}
+
+            <button onClick={() => onDelete(post.id)}>Delete</button>
+            <button onClick={() => onEdit(post.id)}>Edit</button>
+
+            {isAdmin && (
+                <div className="post-actions">
+                    <button onClick={() => onEdit(post.id)}>Edit</button>
+                    <button onClick={() => onDelete(post.id)}>Delete</button>
+                </div>
+            )}
+        </li>
+    )
+    return (
+        <ul>{list}</ul>
+    )  
+}
+
+export default PostList;
