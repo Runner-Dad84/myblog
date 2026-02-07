ddb9f206db19a287dae04abdbe61f8ce487dc240
Runner-Dad84 <vanhoogie@gmail.com>
2026-02-06 21:44:38 -0500

    fix port number to match backend 3011

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:311',
+        target: 'http://localhost:3011',
         changeOrigin: true,
       },
     },
