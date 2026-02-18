feecf7d052a123f8399d1028e2fae16c8fc7293e
Runner-Dad84 <vanhoogie@gmail.com>
2026-02-17 21:45:12 -0500

    onLogout handler

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,14 +5,41 @@ import AdminPostPage from "./pages/AdminPostPage";
 export default function App() {
   const [user, setUser] = useState(null);
 
-  function handleLogout() {
-    setUser(null);
+  async function onLogout() {
+    
+    try {
+      const res = await fetch("/api/auth/logout", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        credentials: "include"
+      })
+      if (!res.ok) {
+        throw new Error('Logtout Failed');
+      } 
+      setUser(null); 
+      } catch (err) {
+      console.error(err);
+    }
   }
+
+
+  
+  /*
+  function handlePostCreated(newPost) {
+    try{
+      const res = await fetch("/api/post/create", {")
+    }
+
+  
+  }
+ */
+
+
 
   return (
     <div>
       {user ? (
-        <AdminPostPage user={user} onLogout={handleLogout} />
+        <AdminPostPage user={user} onLogout={onLogout} />
       ) : (
         <AuthPage onAuthSuccess={setUser} />
       )}
